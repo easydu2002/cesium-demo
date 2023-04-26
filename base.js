fdfb0566ad27ced1be0d90a15d38ad50ab8cb53d
easydu <qingsongdu2002@qq.com>
2023-04-26 10:20:37 +0800

    基础环境配置

---
+++ base.js
@@ -0,0 +1,9 @@
+/// <reference path="./Cesium.d.ts"/>
+
+const viewer = new Cesium.Viewer('cesiumContainer', {
+  scene3DOnly: true,
+  selectionIndicator: false,
+  baseLayerPicker: false,
+});
+
+Cesium.Camera.DEFAULT_VIEW_RECTANGLE = Cesium.Rectangle.fromDegrees(80, 22, 130, 50)
