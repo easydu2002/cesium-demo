bbe5b1e481db2a22af9a3f886b9a621a37f118d1
easydu <qingsongdu2002@qq.com>
2023-04-26 18:53:07 +0800

    更换影像

--- base.js
+++ base.js
@@ -4,6 +4,12 @@ const viewer = new Cesium.Viewer('cesiumContainer', {
   scene3DOnly: true,
   selectionIndicator: false,
   baseLayerPicker: false,
+  imageryProvider: Cesium.UrlTemplateImageryProvider({
+    url : 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+    subdomains : ['0','1','2','3'],
+    tilingScheme : new Cesium.WebMercatorTilingScheme()
+  })
+
 });
 
 viewer.scene.debugShowFramesPerSecond = true
